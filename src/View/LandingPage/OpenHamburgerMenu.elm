de3c9286b9b64cf76f2942e9740a5b15ff37d005
Alexander Binaei <alex.binaei@carpe.io>
2020-12-14 12:08:41 -0800

    modal with custom background img

--- src/View/LandingPage/OpenHamburgerMenu.elm
+++ src/View/LandingPage/OpenHamburgerMenu.elm
@@ -2,42 +2,85 @@ module View.LandingPage.OpenHamburgerMenu exposing (view)
 
 import Html exposing (Html)
 import Html.Attributes exposing (class)
-import Msg.Msg exposing (Msg)
+import Html.Events exposing (onClick)
+import Model.State exposing (IsOpen(..))
+import Msg.Msg exposing (Msg(..))
 
 
 view : Html Msg
 view =
-    Html.div
-        [ class "hero has-background-grey-lighter is-fullheight"
+    hero
+
+
+hero : Html Msg
+hero =
+    Html.section
+        [ class "hero is-fullheight has-bg-img1"
         ]
         [ Html.div
             [ class "hero-body has-text-centered is-family-secondary"
             ]
+            [ modal
+            ]
+        ]
+
+
+modal : Html Msg
+modal =
+    Html.div
+        [ class "modal is-active"
+        ]
+        [ Html.div
+            [ class "modal-background"
+            ]
+            []
+        , Html.div
+            [ class "modal-content"
+            ]
             [ Html.div
-                [ class "container is-fluid"
+                [ class "columns is-multiline has-text-centered is-family-secondary"
                 ]
                 [ Html.div
-                    [ class "columns is-multiline"
+                    [ class "column is-12"
                     ]
-                    [ Html.div
-                        [ class "column is-12"
+                    [ Html.button
+                        [ class "button"
                         ]
-                        [ Html.button
-                            [ class "button"
-                            ]
-                            [ Html.text "Button1"
-                            ]
+                        [ Html.text "button1"
                         ]
-                    , Html.div
-                        [ class "column is-12"
+                    ]
+                , Html.div
+                    [ class "column is-12"
+                    ]
+                    [ Html.button
+                        [ class "button"
                         ]
-                        [ Html.button
-                            [ class "button"
-                            ]
-                            [ Html.text "Button1"
-                            ]
+                        [ Html.text "button2"
+                        ]
+                    ]
+                , Html.div
+                    [ class "column is-12"
+                    ]
+                    [ Html.button
+                        [ class "button"
+                        ]
+                        [ Html.text "button3"
+                        ]
+                    ]
+                , Html.div
+                    [ class "column is-12"
+                    ]
+                    [ Html.button
+                        [ class "button"
+                        ]
+                        [ Html.text "button4"
                         ]
                     ]
                 ]
             ]
+        , Html.button
+            [ class "modal-close is-large"
+            , onClick (ClickedHamburgerMenu Closed)
+            ]
+            []
         ]
