2638e17e726e734ad94b919c322bde763932d3a4
Alexander Binaei <alex.binaei@carpe.io>
2020-12-18 19:06:51 -0800

    Our Story with sections, and a quoted header.

--- src/View/OurStory/OurStory.elm
+++ src/View/OurStory/OurStory.elm
@@ -21,4 +21,64 @@ view model =
                 [ Html.text "Our Story"
                 ]
             ]
+        , bigBoyBody
+        , Html.div
+            [ class "section"
+            ]
+            [ Html.div
+                [ class "container is-max-desktop"
+                ]
+                [ Html.text (String.repeat 30 blockOfText)
+                ]
+            ]
+        , Html.div
+            [ class "section"
+            ]
+            [ Html.div
+                [ class "container is-max-desktop"
+                ]
+                [ Html.text (String.repeat 50 blockOfText)
+                ]
+            ]
+        , Html.div
+            [ class "section"
+            ]
+            [ Html.div
+                [ class "container is-max-desktop"
+                ]
+                [ Html.text (String.repeat 20 blockOfText)
+                ]
+            ]
         ]
+
+
+bigBoy : String
+bigBoy =
+    """
+    "We're out here, looking for a tap in. It really boils down to that.
+    Do I need more? There's nothing else to have."
+    """
+
+
+bigBoyBody : Html Msg
+bigBoyBody =
+    Html.div
+        [ class "section"
+        ]
+        [ Html.div
+            [ class "container is-max-desktop our-story3"
+            ]
+            [ Html.h2
+                [ class "title is-size-4-touch is-size-2-desktop our-story4"
+                ]
+                [ Html.text bigBoy
+                ]
+            ]
+        ]
+
+
+blockOfText : String
+blockOfText =
+    """
+    Big Block of Text.
+    """
